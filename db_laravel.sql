236e069c0c361cadfec4d8937bb2c0dcefde1c0d
Luchonathor <luismimora81@hotmail.com>
2020-09-18 10:26:21 -0500

    base de datos para laravel

---
+++ db_laravel.sql
@@ -0,0 +1,138 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.11-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para prb_sos_laravel
+DROP DATABASE IF EXISTS `prb_sos_laravel`;
+CREATE DATABASE IF NOT EXISTS `prb_sos_laravel` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `prb_sos_laravel`;
+
+-- Volcando estructura para tabla prb_sos_laravel.bodegas
+DROP TABLE IF EXISTS `bodegas`;
+CREATE TABLE IF NOT EXISTS `bodegas` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.bodegas: ~0 rows (aproximadamente)
+DELETE FROM `bodegas`;
+/*!40000 ALTER TABLE `bodegas` DISABLE KEYS */;
+INSERT INTO `bodegas` (`id`, `nombre`, `direccion`, `created_at`, `updated_at`) VALUES
+	(1, 'Bodega 1', 'Dirección de la bodega 1', '2020-09-18 15:17:39', '2020-09-18 15:17:39'),
+	(2, 'Bodega 2', 'Dirección de la bodega 2', '2020-09-18 15:17:48', '2020-09-18 15:17:48'),
+	(3, 'Bodega 3', 'Dirección de la bodega 3', '2020-09-18 15:18:00', '2020-09-18 15:18:00');
+/*!40000 ALTER TABLE `bodegas` ENABLE KEYS */;
+
+-- Volcando estructura para tabla prb_sos_laravel.failed_jobs
+DROP TABLE IF EXISTS `failed_jobs`;
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.failed_jobs: ~0 rows (aproximadamente)
+DELETE FROM `failed_jobs`;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+
+-- Volcando estructura para tabla prb_sos_laravel.migrations
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.migrations: ~5 rows (aproximadamente)
+DELETE FROM `migrations`;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(5, '2014_10_12_000000_create_users_table', 1),
+	(6, '2014_10_12_100000_create_password_resets_table', 1),
+	(7, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(8, '2020_09_18_135231_create_bodegas_table', 1),
+	(9, '2020_09_18_144257_create_productos_table', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Volcando estructura para tabla prb_sos_laravel.password_resets
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.password_resets: ~0 rows (aproximadamente)
+DELETE FROM `password_resets`;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Volcando estructura para tabla prb_sos_laravel.productos
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `codigo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `existencia` int(11) NOT NULL DEFAULT 0,
+  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estado` enum('activo','inactivo','pendiente') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pendiente',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id_bodega` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productos_id_bodega_foreign` (`id_bodega`),
+  CONSTRAINT `productos_id_bodega_foreign` FOREIGN KEY (`id_bodega`) REFERENCES `bodegas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.productos: ~0 rows (aproximadamente)
+DELETE FROM `productos`;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` (`id`, `nombre`, `codigo`, `existencia`, `descripcion`, `estado`, `created_at`, `updated_at`, `id_bodega`) VALUES
+	(1, 'Producto 1', '02225641', 600, 'Nuevo producto 1', 'pendiente', '2020-09-18 15:21:49', '2020-09-18 15:21:49', 1),
+	(2, 'Producto 2', '12356987', 120, 'Nuevo producto 2', 'pendiente', '2020-09-18 15:22:14', '2020-09-18 15:22:14', 2);
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+
+-- Volcando estructura para tabla prb_sos_laravel.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla prb_sos_laravel.users: ~0 rows (aproximadamente)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
